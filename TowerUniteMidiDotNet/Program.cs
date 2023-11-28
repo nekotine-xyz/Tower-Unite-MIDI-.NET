79bf84c140b0ee8ba40d635f9894323a910c9b6d
xelathing <xela@xela.contact>
2023-11-28 08:37:14 -0500

    run a DLL check before running

--- TowerUniteMidiDotNet/Program.cs
+++ TowerUniteMidiDotNet/Program.cs
@@ -1,19 +1,37 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
+using System.Linq;
+using TowerUniteMidiDotNet.Windows;
 
-namespace TowerUniteMidiDotNet.Windows
+static class Program
 {
-	static class Program
-	{
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main()
-		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainWindow());
-		}
-	}
+    // list the required DLLs
+    private static readonly string[] RequiredDlls = {
+        "WindowsInput.dll",
+        "Melanchall.DryWetMidi.dll",
+        "NHotkey.dll",
+        "NHotkey.WindowsForms.dll"
+    };
+
+    [STAThread]
+    static void Main()
+    {
+        // before running the application, check for the required DLLs
+        var missingDlls = RequiredDlls.Where(dll => !File.Exists(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, dll))).ToList();
+        if (missingDlls.Any())
+        {
+            string missingDllsMessage = "The following required libraries are missing:\n" + string.Join("\n", missingDlls) +
+                                        "\n\nPlease ensure you have unpacked all the DLLs included in the archive to the same directory as the executable.";
+            MessageBox.Show(missingDllsMessage, "Missing Libraries", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            // exit the program if DLLs are missing
+            Environment.Exit(1);
+        }
+
+        // if all DLLs are present, continue to run the application
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainWindow());
+    }
 }
